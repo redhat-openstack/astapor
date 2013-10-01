5789d69256fc80738fe87646207364cc091e864d
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 16:25:25 +1000

    test1

--- puppet/modules/quickstack/manifests/storage_backend/gluster.pp
+++ puppet/modules/quickstack/manifests/storage_backend/gluster.pp
@@ -15,11 +15,15 @@ class quickstack::storage_backend::gluster {
   } 
 
   # 1 port per brick - We start with three
-  firewall { '003 gluster bricks incoming':
+  firewall { '002 gluster bricks incoming':
     proto    => 'tcp',
     #dport    => port_range('24009', 6)
     dport    => [ '24009', '24010', '24011' ],
+
+    notify { 'foo':
+      message => range('24009','24009'+6)
+    }
+
     action   => 'accept',
   } 
 }
-
