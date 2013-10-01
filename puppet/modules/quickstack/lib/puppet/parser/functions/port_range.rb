5f7e9082173abb4b3c9c9db4468e3baf8dc8f86c
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 16:13:27 +1000

    mixed

--- puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
@@ -1,9 +1,8 @@
 Puppet::Parser::Functions::newfunction(:port_range, :type => :rvalue,
-  :doc => "Return an array of ports from first port to interval size") do |args|
-    list = []
+	:doc => "Return an array of ports from first port to interval size") do |args|
+	list = []
 	for counter in 0..args[1]-1
 		list << (args[0].to_i + counter).to_s
 	end
-  return list
-end
-
+  	return list
+  end
--- puppet/modules/quickstack/manifests/cinder_storage.pp
+++ puppet/modules/quickstack/manifests/cinder_storage.pp
@@ -28,7 +28,7 @@ class quickstack::cinder_storage (
 
     firewall { '001 gluster bricks incoming':
       proto  => 'tcp',
-      # dport  => port_range('24009', 3) # size($cinder_gluster_peers))
+      # dport  => port_range('24009', size($cinder_gluster_peers))
       dport  => [ '24009', '24010', '24011' ],
       action => 'accept',
     }
