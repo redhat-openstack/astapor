8868c5565d8086c695b3d29f487412e3ac035f0e
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 17:02:59 +1000

    Not using port_range until fixed

--- puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
@@ -1,5 +1,4 @@
 module Puppet::Parser::Functions
-	puts 'here'
 	newfunction(:port_range, :type => :rvalue,
 	:doc => "Return an array of ports from first port to interval size") do |args|
 	list = []
--- puppet/modules/quickstack/manifests/storage_backend/gluster.pp
+++ puppet/modules/quickstack/manifests/storage_backend/gluster.pp
@@ -9,16 +9,16 @@ class quickstack::storage_backend::gluster {
 #  class { 'quickstack::storage_backend::gluster::volume_swift': } 
 
   firewall { '001 RPC and gluster daemon incoming':
-    proto    => 'tcp',
-    dport    => [ '111', '24007', '24008' ],
-    action   => 'accept',
+    proto  => 'tcp',
+    dport  => [ '111', '24007', '24008' ],
+    action => 'accept',
   } 
 
   # 1 port per brick - We start with three
   firewall { '002 gluster bricks incoming':
-    proto    => 'tcp',
-    dport    => port_range('24009', '6'),
-    #dport    => [ '24009', '24010', '24011' ],
-    action   => 'accept',
+    proto  => 'tcp',
+    #dport  => port_range('24009', '6'),
+    dport    => [ '24009', '24010', '24011' ],
+    action => 'accept',
   } 
 }
