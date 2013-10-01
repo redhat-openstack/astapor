0c3b28deb8469e6ce10c86da8c87a90f513db3f6
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 16:51:15 +1000

    debuging port_range

--- puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
@@ -2,7 +2,8 @@ module Puppet::Parser::Functions
 	newfunction(:port_range, :type => :rvalue,
 	:doc => "Return an array of ports from first port to interval size") do |args|
 	list = []
-	for counter in 0..(args[1].to_i-1)
+	p args[1].class
+	for counter in 0..(args[1].to_i - 1)
 		list << (args[0].to_i + counter).to_s
 	end
   	return list
