0ae02a8654f0c8b4c939d677a72eb51f735253a8
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 13:45:57 +1000

    wrong volume names

--- puppet/modules/quickstack/manifests/storage_backend.pp
+++ puppet/modules/quickstack/manifests/storage_backend.pp
@@ -6,7 +6,20 @@ class quickstack::storage_backend (
     'gluster': {  
       class { 'gluster::server': }
 
-      class { 'quickstack::storage_backend::gluster::volume::cinder': }
+      class { 'quickstack::storage_backend::gluster::volume_cinder': }
+
+      firewall { '001 RPC and gluster daemon incoming':
+        proto    => 'tcp',
+        dport    => [ '111', '24007', '24008' ],
+        action   => 'accept',
+      } 
+
+      # 1 port per brick - We start with three
+      firewall { '003 gluster bricks incoming':
+        proto    => 'tcp',
+        dport    => [ '24009', '24010', '24011'],
+        action   => 'accept',
+      } 
     }
     'iscsi': {
     }
@@ -16,7 +29,20 @@ class quickstack::storage_backend (
     'gluster': {
       class { 'gluster::server': }
 
-      class { 'quickstack::storage_backend::gluster::volume::glance': }
+      class { 'quickstack::storage_backend::gluster::volume_glance': } 
+
+      firewall { '001 RPC and gluster daemon incoming':
+        proto    => 'tcp',
+        dport    => [ '111', '24007', '24008' ],
+        action   => 'accept',
+      } 
+
+      # 1 port per brick - We start with three
+      firewall { '003 gluster bricks incoming':
+        proto    => 'tcp',
+        dport    => [ '24009', '24010', '24011'],
+        action   => 'accept',
+      } 
     }
     'iscsi': {
     }
