465e9dd85838127f039a97e046fa5a473a5d5b58
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-01 16:54:32 +1000

    trying preassignment with interval border

--- puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/port_range.rb
@@ -3,7 +3,8 @@ module Puppet::Parser::Functions
 	:doc => "Return an array of ports from first port to interval size") do |args|
 	list = []
 	p args[1].class
-	for counter in 0..(args[1].to_i - 1)
+	last -= args[1].to_i
+	for counter in 0..last
 		list << (args[0].to_i + counter).to_s
 	end
   	return list
