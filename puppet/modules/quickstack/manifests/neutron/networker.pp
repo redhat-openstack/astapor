28797a63c41313d9798f6aa0aacf9638904cca4c
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-02 14:50:18 +1000

    Added br-ex (vs_bridge) creation that went missing after cleaning pull request

--- puppet/modules/quickstack/manifests/neutron/networker.pp
+++ puppet/modules/quickstack/manifests/neutron/networker.pp
@@ -10,6 +10,18 @@ class quickstack::neutron::networker (
   $public_interface             = $quickstack::params::public_interface,
   $verbose                      = $quickstack::params::verbose,
 ) inherits quickstack::params {
+    vs_bridge { 'br-ex':
+      provider => ovs_redhat,
+      ensure   => present,
+    } -> 
+    vs_port { 'external':
+      bridge    => 'br-ex',
+      interface => $public_interface,
+      keep_ip   => true,
+      sleep     => '30',
+      provider  => ovs_redhat,
+      ensure    => present,
+    }
 
     class { '::neutron':
         verbose               => true,
