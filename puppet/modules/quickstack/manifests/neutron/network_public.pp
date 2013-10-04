c3409c131bd979a9655647393923dede2ce33177
Gilles Dubreuil <gdubreui@redhat.com>
2013-10-04 13:57:00 +1000

    Commented out the neutron_subnet section

--- puppet/modules/quickstack/manifests/neutron/network_public.pp
+++ puppet/modules/quickstack/manifests/neutron/network_public.pp
@@ -16,12 +16,12 @@ class quickstack::neutron::network_public (
 
     # Not ready yet:
     # https://github.com/stackforge/puppet-neutron/blob/b6b50a97e060261ce66817ba8255ae4d710e0009/lib/puppet/type/neutron_subnet.rb#L29
-    neutron_subnet { 'public_subnet':
-        ensure           => 'present',
-        cidr             => $cidr,
-        gateway_ip       => $gateway_ip,
-        allocation_pools => "start=${allocation_pools_start},end=${allocation_pools_end}",
-        network_name     => $network_name,
-        tenant_name      => $tenant_name,
-    }
+    #neutron_subnet { 'public_subnet':
+    #    ensure           => 'present',
+    #    cidr             => $cidr,
+    #    gateway_ip       => $gateway_ip,
+    #    allocation_pools => "start=${allocation_pools_start},end=${allocation_pools_end}",
+    #    network_name     => $network_name,
+    #    tenant_name      => $tenant_name,
+    #}
 }
