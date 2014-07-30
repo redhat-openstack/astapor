f665aaa6527830d67fbd97db9a0e78bd3b5f5102
Jiri Stransky <jistr@redhat.com>
2014-07-30 14:04:10 +0200

    BZ#1122701 - Incoming ports are wide open
    https://bugzilla.redhat.com/show_bug.cgi?id=1122701
    
    Save firewall rules before disabling firewalld.

--- puppet/modules/quickstack/manifests/openstack_common.pp
+++ puppet/modules/quickstack/manifests/openstack_common.pp
@@ -7,8 +7,12 @@ class quickstack::openstack_common {
           ensure => present, }
   }
 
-  # Stop firewalld since everything uses iptables
-  # for now (same as packstack did)
+  # Stop firewalld since everything uses iptables for now (same as
+  # packstack did), but save and restore the initial firewall rules
+  # (allow loopback, reject at the end of the chain etc.)
+  exec { "iptables-save-before-disabling-firewalld":
+    command => "/usr/sbin/iptables-save > /etc/sysconfig/iptables"
+  } ->
   service { "firewalld":
     ensure => "stopped",
     enable => false,
