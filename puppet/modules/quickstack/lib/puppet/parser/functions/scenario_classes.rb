a6b79d833bb2be49244436e7d147ad7468a2ef5e
Gilles Dubreuil <gilles@redhat.com>
2014-12-05 16:27:48 +1100

    loop debug

--- puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
@@ -31,7 +31,8 @@ EOS
 
     scenario = arguments[0] ||= ''
     scenarii = arguments[1] ||= []
-
+   p scenario
+   p scenarii
     raise(Puppet::ParseError, "Missing argumets") if @scenario.empty? || @scenarii.empty?
 
     list = details(scenarii[scenario]['roles'], scenarii) if scenarii[scenario]['roles']
