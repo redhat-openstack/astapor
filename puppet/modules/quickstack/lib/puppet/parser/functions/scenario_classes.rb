a8ca790ef0429e56edddbd835ea3d6ef2c4bb43b
Gilles Dubreuil <gilles@redhat.com>
2014-12-05 16:54:49 +1100

    testing2

--- puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
@@ -34,6 +34,7 @@ EOS
     raise(Puppet::ParseError, "Missing argumets") if scenario.empty? || scenarii.empty?
 
     list = details(scenarii[scenario]['roles'], scenarii) if scenarii[scenario]['roles']
+    p list
     list.concat(scenarii[scenario]['classes']) if scenarii[scenario]['classes']
     list.flatten! unless list.empty?
   end
