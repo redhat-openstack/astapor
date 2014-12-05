e318b229fd64e46c8ea3760cf25664139e9f5df8
Gilles Dubreuil <gilles@redhat.com>
2014-12-05 21:25:18 +1100

    works using parser=future

--- puppet/modules/quickstack/manifests/init.pp
+++ puppet/modules/quickstack/manifests/init.pp
@@ -2,9 +2,8 @@
 class quickstack (
 ) inherits quickstack::params {
 
-  $list = join(scenario_classes("$scenario", $scenarii))
+  $list = scenario_classes("$scenario", $scenarii)
   notify {"running $scenario":}
   notify {"$list":}
-  # $list.each |$e| { include "$e" }
-  include $list
+  $list.each |$e| { include "$e" }
 }
