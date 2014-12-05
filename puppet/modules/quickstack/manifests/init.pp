27a8bd1ea59ab432e9d7145015666a276af170ae
Gilles Dubreuil <gilles@redhat.com>
2014-12-05 16:50:38 +1100

    testing1

--- puppet/modules/quickstack/manifests/init.pp
+++ puppet/modules/quickstack/manifests/init.pp
@@ -1,12 +1,11 @@
 # Quickstack init
 class quickstack (
-  $scenario = undef,
 ) inherits quickstack::params {
 
   notify {"running $scenario":}
   # notify {"test $scenarii":}
 
   $list = scenario_classes($scenario, $scenarii)
-  notify {$list:}
+  notify {"$list":}
   include $list
 }
