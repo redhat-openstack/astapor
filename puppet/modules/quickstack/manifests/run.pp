9dde3ce761a6f823ee0e5d66212f4a869d5a3d09
Gilles Dubreuil <gilles@redhat.com>
2014-12-06 17:49:06 +1100

    Adding scenarii pp

--- puppet/modules/quickstack/manifests/run.pp
+++
@@ -1,6 +0,0 @@
-# Quickstack init
-class quickstack::run (
-) inherits quickstack::params {
-
-  notify {"running run":}
-}
--- puppet/modules/quickstack/manifests/run2.pp
+++
@@ -1,6 +0,0 @@
-# Quickstack init
-class quickstack::run2 (
-) inherits quickstack::params {
-
-  notify {"running run2":}
-}
