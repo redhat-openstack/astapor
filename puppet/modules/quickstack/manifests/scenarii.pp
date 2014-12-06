3996f5abd5e706aa66875c7452ab06d2891c4a63
Gilles Dubreuil <gilles@redhat.com>
2014-12-06 17:50:21 +1100

    Adding scenarii pp file

---
+++ puppet/modules/quickstack/manifests/scenarii.pp
@@ -0,0 +1,9 @@
+# Quickstack scenarii
+class quickstack::scenarii ()
+  inherits quickstack::params {
+    notify {"Available scenarios":}
+    $scenarii.each |$e| {
+      notify {"$e":}
+    }
+  }
+}
