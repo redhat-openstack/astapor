fc0b0532caebfeb4a9249f50a553d803439f0501
Gilles Dubreuil <gilles@redhat.com>
2014-12-06 21:46:37 +1100

    externalized scenario core 5

--- bin/scenario_classes.rb
+++ bin/scenario_classes.rb
@@ -1,30 +1,37 @@
 #!/bin/env ruby
-scenario = ARGV[0]
-SCENARII = {"compute"=>{"desc"=>"Nova Compute (no network)", "classes"=>["quickstack::compute_common"]}, "compute-neutron"=>{"desc"=>"Nova Compute, Neutron Network", "classes"=>["quickstack::neutron::nova"], "roles"=>["compute", "neutron-client-ML2-OVS"]}, "custom"=>{"desc"=>"Scenario from a list of classes and/or roles", "classes"=>["quickstack::compute_common"], "roles"=>[]}, "HA"=>{"desc"=>"HA Controllers (3+), Neutron Network agents", "classes"=>nil, "roles"=>["HA-controller", "neutron-network-agents", "PCS-neutron"]}, "HA-AIO"=>{"desc"=>"HA All in One (1), HA Controllers, Neutron network agents, Compute", "classes"=>["quickstack::compute_common"], "roles"=>["HA"]}, "HA-controller"=>{"desc"=>"HA Controllers (3+), No network", "classes"=>["quickstack::openstack_common", "quickstack::pacemaker::common", "quickstack::pacemaker::params", "quickstack::pacemaker::keystone", "quickstack::pacemaker::swift", "quickstack::pacemaker::load_balancer", "quickstack::pacemaker::memcached", "quickstack::pacemaker::qpid", "quickstack::pacemaker::rabbitmq", "quickstack::pacemaker::glance", "quickstack::pacemaker::nova", "quickstack::pacemaker::heat", "quickstack::pacemaker::cinder", "quickstack::pacemaker::horizon", "quickstack::pacemaker::galera"]}, "neutron-client-ML2-OVS"=>{"desc"=>"Neutron network layer, ML2 plugin, OVS agent", "classes"=>["quickstack::neutron", "quickstack::neutron::plugins::ml2", "quickstack::neutron::agents::ovs"]}, "neutron-network-agents"=>{"desc"=>"Neutron network agents", "classes"=>["quickstack::neutron::agents::dhcp", "quickstack::neutron::agents::l3", "quickstack::neutron::agents::lbass", "quickstack::neutron::services::fwaas"], "roles"=>["neutron-client-ML2-OVS"]}, "PCS-neutron"=>{"desc"=>"Pacemaker Neutron", "classes"=>["quickstack::pacemaker::neutron"], "roles"=>["neutron-client-ML2-OVS"]}, "legacy-controller-NHA"=>{"desc"=>"NHA Controller (1)", "classes"=>["quickstack::neutron::controller"]}, "legacy-ceutron-networker"=>{"desc"=>"Neutron Cliente Agents", "classes"=>["quickstack::neutron::networker"]}, "legacy-compute-neutron"=>{"desc"=>"Nova Compute, Neutron NHA client", "classes"=>["quickstack::neutron::compute"]}}
 
-class Scene
+require "hiera"
+require "hiera/config"
+require "hiera/scope"
+require "/usr/share/openstack-puppet/modules/module-data/lib/hiera/backend/module_data_backend"
+require '/usr/share/openstack-foreman-installer/puppet/modules/quickstack/lib/puppetx/redhat/scenario.rb'
+
+class Hiera::Config
   class << self
-    def get_all_classes(roles, scenarii)
-      list = Array.new
-      roles.each do |role|
-        if scenarii[role]
-          if scenarii[role]['roles']
-            list << Scene.get_all_classes(scenarii[role]['roles'], scenarii)
-          end
-          if scenarii[role]['classes']
-            list << scenarii[role]['classes']
-          end
-        end
-      end
-      list
-      list.flatten!.uniq! unless list.empty?
+    alias :old_load :load unless respond_to?(:old_load)
+
+    def load(source)
+      old_load(source)
+
+      @config[:backends] << "module_data" unless @config[:backends].include?("module_data")
+
+      @config
     end
   end
 end
 
-deps=[]
-deps << Scene.get_all_classes(SCENARII[scenario]['roles'], SCENARII) if SCENARII[scenario]['roles']
-deps << SCENARII[scenario]['classes'] if SCENARII[scenario]['classes']
-p deps
-deps.flatten! unless deps.empty?
-deps
+context=
+
+scenario = ARGV[0]
+scenarii_test = {"compute"=>{"desc"=>"Nova Compute (no network)", "classes"=>["quickstack::compute_common"]}, "compute-neutron"=>{"desc"=>"Nova Compute, Neutron Network", "classes"=>["quickstack::neutron::nova"], "roles"=>["compute", "neutron-client-ML2-OVS"]}, "custom"=>{"desc"=>"Scenario from a list of classes and/or roles", "classes"=>["quickstack::compute_common"], "roles"=>[]}, "HA"=>{"desc"=>"HA Controllers (3+), Neutron Network agents", "classes"=>nil, "roles"=>["HA-controller", "neutron-network-agents", "PCS-neutron"]}, "HA-AIO"=>{"desc"=>"HA All in One (1), HA Controllers, Neutron network agents, Compute", "classes"=>["quickstack::compute_common"], "roles"=>["HA"]}, "HA-controller"=>{"desc"=>"HA Controllers (3+), No network", "classes"=>["quickstack::openstack_common", "quickstack::pacemaker::common", "quickstack::pacemaker::params", "quickstack::pacemaker::keystone", "quickstack::pacemaker::swift", "quickstack::pacemaker::load_balancer", "quickstack::pacemaker::memcached", "quickstack::pacemaker::qpid", "quickstack::pacemaker::rabbitmq", "quickstack::pacemaker::glance", "quickstack::pacemaker::nova", "quickstack::pacemaker::heat", "quickstack::pacemaker::cinder", "quickstack::pacemaker::horizon", "quickstack::pacemaker::galera"]}, "neutron-client-ML2-OVS"=>{"desc"=>"Neutron network layer, ML2 plugin, OVS agent", "classes"=>["quickstack::neutron", "quickstack::neutron::plugins::ml2", "quickstack::neutron::agents::ovs"]}, "neutron-network-agents"=>{"desc"=>"Neutron network agents", "classes"=>["quickstack::neutron::agents::dhcp", "quickstack::neutron::agents::l3", "quickstack::neutron::agents::lbass", "quickstack::neutron::services::fwaas"], "roles"=>["neutron-client-ML2-OVS"]}, "PCS-neutron"=>{"desc"=>"Pacemaker Neutron", "classes"=>["quickstack::pacemaker::neutron"], "roles"=>["neutron-client-ML2-OVS"]}, "legacy-controller-NHA"=>{"desc"=>"NHA Controller (1)", "classes"=>["quickstack::neutron::controller"]}, "legacy-ceutron-networker"=>{"desc"=>"Neutron Cliente Agents", "classes"=>["quickstack::neutron::networker"]}, "legacy-compute-neutron"=>{"desc"=>"Nova Compute, Neutron NHA client", "classes"=>["quickstack::neutron::compute"]}}
+
+raise(Puppet::ParseError, "Missing argumets") if scenario.empty? || scenarii.empty?
+
+case context
+when 'dry'
+  result = Scenario.Scene.all_classes(scenario, scenarii_test)
+  # expect - Can we use rspec from here?
+when 'run'
+  scenarii = hiera
+  Scenario.Scene.all_classes(scenario, scenarii)
+end
