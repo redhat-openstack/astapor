62b8d616dc35f57664e6654b08b8a05eefb290c3
Gilles Dubreuil <gilles@redhat.com>
2014-12-06 21:23:17 +1100

    externalized scenario core 3

--- puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
@@ -1,33 +1,6 @@
 Puppet::Parser::Functions.autoloader.loadall
-module Scenario
-  class Scene
-    class << self
-      def get_all_classes(roles, scenarii)
-        list = []
-        roles.each do |role|
-          if scenarii[role]
-            if scenarii[role]['roles']
-              list << self.get_all_classes(scenarii[role]['roles'], scenarii)
-            end
-            if scenarii[role]['classes']
-              list << scenarii[role]['classes']
-            end
-          end
-        end
-        list.flatten!.uniq! unless list.empty?
-        list.delete_if {|x| x == nil }
-      end
 
-      def all_classes(scenario, scenarii)
-        list = []
-        list = self.get_all_classes(scenarii[scenario]['roles'], scenarii) if scenarii[scenario]['roles']
-        list << scenarii[scenario]['classes'] if scenarii[scenario]['classes']
-        list.flatten! unless list.empty?
-        list
-      end
-    end
-  end
-end
+load '/usr/share/openstack-foreman-installer/lib/scenario'
 
 module Puppet::Parser::Functions
 
