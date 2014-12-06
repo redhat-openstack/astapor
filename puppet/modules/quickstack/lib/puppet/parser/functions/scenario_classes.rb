05ea77e0c81ccdd3548544a2f890dc6f141b2d03
Gilles Dubreuil <gilles@redhat.com>
2014-12-06 21:15:40 +1100

    externalized scenario core 2

--- puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
@@ -1,29 +1,30 @@
 Puppet::Parser::Functions.autoloader.loadall
-
-class Scene
-  class << self
-    def get_all_classes(roles, scenarii)
-      list = []
-      roles.each do |role|
-        if scenarii[role]
-          if scenarii[role]['roles']
-            list << Scene.get_all_classes(scenarii[role]['roles'], scenarii)
-          end
-          if scenarii[role]['classes']
-            list << scenarii[role]['classes']
+module Scenario
+  class Scene
+    class << self
+      def get_all_classes(roles, scenarii)
+        list = []
+        roles.each do |role|
+          if scenarii[role]
+            if scenarii[role]['roles']
+              list << self.get_all_classes(scenarii[role]['roles'], scenarii)
+            end
+            if scenarii[role]['classes']
+              list << scenarii[role]['classes']
+            end
           end
         end
+        list.flatten!.uniq! unless list.empty?
+        list.delete_if {|x| x == nil }
       end
-      list.flatten!.uniq! unless list.empty?
-      list.delete_if {|x| x == nil }
-    end
 
-    def all_classes(scenario, scenarii)
-      list = []
-      list = Scene.get_all_classes(scenarii[scenario]['roles'], scenarii) if scenarii[scenario]['roles']
-      list << scenarii[scenario]['classes'] if scenarii[scenario]['classes']
-      list.flatten! unless list.empty?
-      list
+      def all_classes(scenario, scenarii)
+        list = []
+        list = self.get_all_classes(scenarii[scenario]['roles'], scenarii) if scenarii[scenario]['roles']
+        list << scenarii[scenario]['classes'] if scenarii[scenario]['classes']
+        list.flatten! unless list.empty?
+        list
+      end
     end
   end
 end
@@ -42,6 +43,6 @@ EOS
     scenarii = arguments[1] ||= {}
     raise(Puppet::ParseError, "Missing argumets") if scenario.empty? || scenarii.empty?
 
-    Scene.all_classes(scenario, scenarii)
+    Scenario.Scene.all_classes(scenario, scenarii)
   end
 end
