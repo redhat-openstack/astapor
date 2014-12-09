2c57a52864b3ef53777a931d31dfb6e223a8a2a5
Gilles Dubreuil <gilles@redhat.com>
2014-12-09 11:31:09 +1100

    scene layout

--- puppet/modules/quickstack/manifests/scene.pp
+++ puppet/modules/quickstack/manifests/scene.pp
@@ -4,9 +4,15 @@ class quickstack::scene (
 
   $modules = scenario_classes("$scenario", $scenarii)
 
+  notify {"quickstack::scene: Available Scenarios":}
+  ->
   notify {"$scenarii":}
+
+  notify {"quickstack::params::scenario":}
   ->
   notify {"$scenario":}
+
+  notify {"Scenario's Puppet classes":}
   ->
   notify {"$modules":}
 }
