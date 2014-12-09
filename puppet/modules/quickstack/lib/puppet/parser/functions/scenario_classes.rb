808e008ad61939b36b5f7e797638b0dd63e4236a
Gilles Dubreuil <gilles@redhat.com>
2014-12-09 11:23:45 +1100

    trying different syntax for loading

--- puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
+++ puppet/modules/quickstack/lib/puppet/parser/functions/scenario_classes.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppetx', 'redhat', 'scenario.rb'))
+#require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'puppetx', 'redhat', 'scenario.rb'))
 
 Puppet::Parser::Functions.autoloader.loadall
 
