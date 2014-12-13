4937c5f375e0ab009c4e795282993958650fcda1
Gilles Dubreuil <gilles@redhat.com>
2014-12-13 21:46:26 +1100

    Decoupling pacemaker/params for neutron
      Added new pacemaker::base
      Inherits done so common -> base -> params
      This way variable propagation is safe and easier to manage
    
      Any variable defined but not assigned by either Hiera or ENC (Foreman)
      will automatically trigger an error, like $foo in the following:
    
      class modulexyz (
        $foo,
      ) {}
    
      $ puppet apply -e "include modulexyz"
      Error: Must pass foo to Class[Modulexyz] at line 1 on node
    
      This can help save lots of testing and frustration with nil values comming up
      the stack
    
      Also added nosql_vip params to hiera
    
      YAML data update and default reorg

--- puppet/modules/quickstack/manifests/pacemaker/params.pp
+++ puppet/modules/quickstack/manifests/pacemaker/params.pp
@@ -105,29 +105,4 @@ class quickstack::pacemaker::params (
   $local_bind_addr = find_ip("$private_network",
                             "$private_iface",
                             "$private_ip")
-  $pcmk_bind_addr = find_ip("$pcmk_network",
-                            "$pcmk_iface",
-                            "$pcmk_ip")
-
-  include quickstack::pacemaker::common
-  include quickstack::pacemaker::load_balancer
-  include quickstack::pacemaker::galera
-  include quickstack::pacemaker::qpid
-  include quickstack::pacemaker::keystone
-  include quickstack::pacemaker::swift
-  include quickstack::pacemaker::glance
-  include quickstack::pacemaker::nova
-  include quickstack::pacemaker::cinder
-  include quickstack::pacemaker::heat
-
-  Class['::quickstack::pacemaker::common'] ->
-  Class['::quickstack::pacemaker::load_balancer'] ->
-  Class['::quickstack::pacemaker::galera'] ->
-  Class['::quickstack::pacemaker::qpid'] ->
-  Class['::quickstack::pacemaker::keystone'] ->
-  Class['::quickstack::pacemaker::swift'] ->
-  Class['::quickstack::pacemaker::glance'] ->
-  Class['::quickstack::pacemaker::nova'] ->
-  Class['::quickstack::pacemaker::cinder'] ->
-  Class['::quickstack::pacemaker::heat']
 }
