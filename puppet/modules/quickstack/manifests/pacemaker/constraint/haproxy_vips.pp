e99f125c1d9fa7c760deedbdf3f490e62247e000
Jason Guiditta <jguiditt@redhat.com>
2015-02-12 17:10:13 -0500

    BZ #1189921 - More ordering fixes for haproxy/vip constraints.
    https://bugzilla.redhat.com/show_bug.cgi?id=1189921
    
    Turns out the colocation for these is set up in the opposite order of every
    other one we use, so need to rework how we call these.
    
    (cherry picked from commit d72968d6995afae7952b3a2eaee06f9dd9f193ab)

--- puppet/modules/quickstack/manifests/pacemaker/constraint/haproxy_vips.pp
+++ puppet/modules/quickstack/manifests/pacemaker/constraint/haproxy_vips.pp
@@ -9,30 +9,30 @@ define quickstack::pacemaker::constraint::haproxy_vips(
   ) {
 
   Quickstack::Pacemaker::Resource::Generic['haproxy'] ->
-  quickstack::pacemaker::constraint::typical{ "haproxy-${pcmk_group}-pub-const" :
+  quickstack::pacemaker::constraint::atypical{ "haproxy-${pcmk_group}-pub-const" :
     first_resource  => "ip-${pcmk_group}-pub-${public_vip}",
     second_resource => "haproxy-clone",
   }
   Quickstack::Pacemaker::Resource::Ip["ip-${pcmk_group}-pub"] ->
-  Quickstack::Pacemaker::Constraint::Typical["haproxy-${pcmk_group}-pub-const"]
+  Quickstack::Pacemaker::Constraint::Atypical["haproxy-${pcmk_group}-pub-const"]
 
   if ( $public_vip != $private_vip ) {
     Quickstack::Pacemaker::Resource::Generic['haproxy'] ->
-    quickstack::pacemaker::constraint::typical{ "haproxy-${pcmk_group}-prv-const" :
+    quickstack::pacemaker::constraint::atypical{ "haproxy-${pcmk_group}-prv-const" :
       first_resource  => "ip-${pcmk_group}-prv-${private_vip}",
       second_resource => "haproxy-clone",
     }
     Quickstack::Pacemaker::Resource::Ip["ip-${pcmk_group}-prv"] ->
-    Quickstack::Pacemaker::Constraint::Typical["haproxy-${pcmk_group}-prv-const"]
+    Quickstack::Pacemaker::Constraint::Atypical["haproxy-${pcmk_group}-prv-const"]
   }
 
   if ( ($admin_vip != $private_vip) and ($admin_vip != $public_vip) ) {
     Quickstack::Pacemaker::Resource::Generic['haproxy'] ->
-    quickstack::pacemaker::constraint::typical{ "haproxy-${pcmk_group}-adm-const" :
+    quickstack::pacemaker::constraint::atypical{ "haproxy-${pcmk_group}-adm-const" :
       first_resource  => "ip-${pcmk_group}-adm-${admin_vip}",
       second_resource => "haproxy-clone",
     }
     Quickstack::Pacemaker::Resource::Ip["ip-${pcmk_group}-adm"] ->
-    Quickstack::Pacemaker::Constraint::Typical["haproxy-${pcmk_group}-adm-const"]
+    Quickstack::Pacemaker::Constraint::Atypical["haproxy-${pcmk_group}-adm-const"]
   }
 }
