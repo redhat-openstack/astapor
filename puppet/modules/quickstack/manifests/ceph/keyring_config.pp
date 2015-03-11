9caf9fa9005c55485f430802812ecb5b01aa39b3
Crag Wolfe <cwolfe@redhat.com>
2015-03-11 00:19:40 -0400

    BZ #1199625 - additional ceph.conf settings
    https://bugzilla.redhat.com/show_bug.cgi?id=1199625
    
    Adds two optional osd parameters.  Adds the ability to include the
    [osd] and/or an [client.radosgw.gateway] sections in ceph.conf.  Adds
    the ability to manage the rgw keyring file (similar to volumes and
    images keyrings).  Adds a "manage_ceph_conf" boolean parameter so that
    /etc/ceph/ceph.conf may or may not be written by puppet.  Adds an
    extra_conf_lines array parameter so that arbirtrary additions may be
    made to ceph.conf.  Renames compute param from
    ceph_osd_pool_default_size to ceph_osd_pool_size for consistency.

--- puppet/modules/quickstack/manifests/ceph/keyring_config.pp
+++ puppet/modules/quickstack/manifests/ceph/keyring_config.pp
@@ -1,9 +1,17 @@
 define quickstack::ceph::keyring_config (
   $key = '',
+  $keyring_filename = '',
+  $caps_mon = 'allow r',
+  $caps_osd = "allow class-read object_prefix rbd_children, allow rwx pool=${title}",
 ) {
   $keyring_name = $title
+  if $keyring_filename {
+    $_keyring_filename = $keyring_filename
+  } else {
+    $_keyring_filename = "/etc/ceph/ceph.client.${keyring_name}.keyring"
+  }
   file { "etc-ceph-keyring-${keyring_name}":
-    path => "/etc/ceph/ceph.client.${keyring_name}.keyring",
+    path => $_keyring_filename,
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
