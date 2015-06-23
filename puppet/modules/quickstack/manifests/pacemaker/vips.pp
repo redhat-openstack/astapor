2fc78283416ec50b8943030a8cc253648661ccdf
Crag Wolfe <cwolfe@redhat.com>
2015-06-23 18:41:26 -0400

    Make sure vips are colo before the usual i-am-vip checks.
    Previously, there were rare (but not rare enough) occaisons where two
    nodes would pass a condition such as
    Exec[i-am-keystone-vip-OR-keystone-is-up-on-vip] before the service
    was properly configured.  This could result in say, multiple
    first-time db_sync's happening near simultaneously causing errors.
    
    The problem was that we add colocation constraints after vip's.  This
    meant that seconds after vips and then the colocation were created, a
    vip could migrate from one node to another.  This patch addresses the
    issue by making sure the vips have "settled" on the same node before
    any i-am-...-vip checks take place.

--- puppet/modules/quickstack/manifests/pacemaker/vips.pp
+++ puppet/modules/quickstack/manifests/pacemaker/vips.pp
@@ -19,6 +19,12 @@ define quickstack::pacemaker::vips(
     quickstack::pacemaker::constraint::colocation { "ip-${pcmk_group}-pub-prv-colo" :
       source => "ip-${pcmk_group}-prv-${private_vip}",
       target => "ip-${pcmk_group}-pub-${public_vip}",
+    } ->
+    exec {"wait-for-${pcmk_group}-prv-${private_vip}-ip-${pcmk_group}-pub-${public_vip}":
+      timeout   => 3600,
+      tries     => 360,
+      try_sleep => 10,
+      command   => "/tmp/ha-all-in-one-util.bash vips_on_same_node ${private_vip} ${public_vip}",
     }
   }
 
@@ -31,6 +37,12 @@ define quickstack::pacemaker::vips(
     quickstack::pacemaker::constraint::colocation { "ip-${pcmk_group}-pub-adm-colo" :
       source => "ip-${pcmk_group}-adm-${admin_vip}",
       target => "ip-${pcmk_group}-pub-${public_vip}",
+    } ->
+    exec {"wait-for-ip-${pcmk_group}-adm-${admin_vip}-ip-${pcmk_group}-pub-${public_vip}":
+      timeout   => 3600,
+      tries     => 360,
+      try_sleep => 10,
+      command   => "/tmp/ha-all-in-one-util.bash vips_on_same_node ${admin_vip} ${public_vip}",
     }
   }
 }
